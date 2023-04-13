5463cdc8ce2540c2429e513653db4be9de0f4838
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-13 17:36:34 +0900

    refactor: named importに変更

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,20 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { Stack, App, StackProps } from 'aws-cdk-lib';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
-export class CdkWorkshopStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+export class CdkWorkshopStack extends Stack {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('lambda'),
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('lambda'),
       handler: 'hello.handler',
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, 'Endpoint', {
+    new LambdaRestApi(this, 'Endpoint', {
       handler: hello,
     });
   }
