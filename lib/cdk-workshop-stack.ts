23214548db665469b057dcf2e0170774c0956326
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-13 17:05:30 +0900

    feat: LambdaRestApi コンストラクトをスタックに追加

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -10,6 +11,11 @@ export class CdkWorkshopStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler',
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
     });
   }
 }
